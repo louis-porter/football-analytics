03e52a65e335748e97db2178fce2f1d1bb371b5c
lsp2610 <louis.porter26@gmail.com>
2024-10-30 16:18:11 +0000

    did g10 predictions and a new study

--- plots/gw_predictions.py
+++ plots/gw_predictions.py
@@ -128,7 +128,7 @@ def primary_pred(df):
     ax.axis('off')
 
     ax.set_title(
-        'GW 9: Predictions',
+        'GW 10: Predictions',
         loc='left',
         fontsize=18,
         weight='bold',
